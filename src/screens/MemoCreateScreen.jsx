a6d5574528549acaece807380ec95761db23747e
ShuntaroSuzuki <75567419+ShuntaroSuzuki@users.noreply.github.com>
2021-03-07 13:31:00 +0900

    Save memo on firestore

--- src/screens/MemoCreateScreen.jsx
+++ src/screens/MemoCreateScreen.jsx
@@ -1,22 +1,49 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useState } from 'react';
 import {
  View, TextInput, StyleSheet, Keyboard,
  } from 'react-native';
+ import firebase from 'firebase';
 
 import CircleButton from '../components/CircleButton';
 import KeyboardSafeView from '../components/KeyboardSafeView';
 
 export default function MemoCreateScreen(props) {
     const { navigation } = props;
+    const [bodyText, setBodyText] = useState('');
+
+    function handlePress() {
+      const { currentUser } = firebase.auth();
+      const db = firebase.firestore();
+      const ref = db.collection(`users/${currentUser.uid}/memos`);
+      ref.add({
+        bodyText,
+        updatedAt: new Date(),
+      })
+        .then((docRef) => {
+          console.log('Created!', docRef.id);
+        })
+        .catch((error) => {
+          console.log('Error!', error);
+        });
+      navigation.goBack();
+    }
+
     return (
       <KeyboardSafeView style={styles.container}>
         <View style={styles.inputContainer}>
-          <TextInput value="" multiline style={styles.input} onSubmitEditing={Keyboard.dismiss} />
+          <TextInput
+            value={bodyText}
+            multiline
+            style={styles.input}
+            onSubmitEditing={Keyboard.dismiss}
+            onChangeText={(text) => { setBodyText(text); }}
+            autoFocus
+          />
         </View>
         <CircleButton
           name="check"
-          onPress={() => { navigation.goBack(); }}
+          onPress={handlePress}
         />
       </KeyboardSafeView>
     );
